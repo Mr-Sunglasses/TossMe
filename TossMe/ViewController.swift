68ade454ee448a9d2e40570b0b16b1953c4860c5
Kanishk Pachauri <kanishkpachauri2020@gmail.com>
2021-07-11 22:34:13 +0530

    Created Toss a coin

--- TossMe/ViewController.swift
+++ TossMe/ViewController.swift
@@ -9,11 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var Toss_coin_image: UIImageView!
+    var image_set = [#imageLiteral(resourceName: "Tail_set"),#imageLiteral(resourceName: "Head_set")]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+
     }
-
-
+    
+    @IBAction func Toss_button(_ sender: Any) {
+                Toss_coin_image.image = image_set[Int.random(in: 0..<2)]
+                    }
+    
 }
 
